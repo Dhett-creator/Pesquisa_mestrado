5067cfc4ecbb324060a0becc6f6d28b7c1f547b0
Dhett-creator <tonymarcio21@gmail.com>
2024-09-10 07:16:10 -0300

    Novos scripts adicionados

--- Processamento_filtro_2.1.1.py
+++ Processamento_filtro_2.1.1.py
@@ -1,5 +1,3 @@
-# Esta versão do script é capaz de processar vários arquivos de uma única vez.
-
 import csv
 import os
 import matplotlib.pyplot as plt
@@ -30,22 +28,14 @@ filter_order = 4  # Ordem do filtro
 # Define o número máximo de linhas que serão lidas de cada arquivo.csv
 max_linhas = 125000
 
-# Lista com os caminhos dos arquivos a serem processados
+# Lista com os caminhos dos 3 arquivos a serem processados
 caminhos_arquivos = [
-    "Dados/APD/xx-07-2024/amostra2_1.80/T0059CH4.CSV",
+    "Dados/APD/xx-07-2024/amostra2_1.60/T0030CH4.CSV",
+    "Dados/APD/xx-07-2024/amostra2_1.70/T0040CH4.CSV",
     "Dados/APD/xx-07-2024/amostra2_1.80/T0060CH4.CSV",
-    "Dados/APD/xx-07-2024/amostra2_1.80/T0061CH4.CSV",
-    "Dados/APD/xx-07-2024/amostra2_1.80/T0064CH4.CSV",
-    "Dados/APD/xx-07-2024/amostra2_1.80/T0065CH4.CSV",
-    "Dados/APD/xx-07-2024/amostra2_1.80/T0066CH4.CSV",
-    "Dados/APD/xx-07-2024/amostra2_1.80/T0067CH4.CSV",
-    "Dados/APD/xx-07-2024/amostra2_1.80/T0069CH4.CSV",
-    "Dados/APD/xx-07-2024/amostra2_1.80/T0070CH4.CSV",
-    "Dados/APD/xx-07-2024/amostra2_1.80/T0071CH4.CSV",
-    # Adicione mais caminhos conforme necessário
 ]
 
-# Processamento de múltiplos arquivos
+# Processamento de múltiplos arquivos (apenas 3)
 for idx, caminho_csv in enumerate(caminhos_arquivos, start=1):
     # Lista para armazenar os dados que estão na 2ª coluna do arquivo de origem
     dados_col2 = []
@@ -80,10 +70,10 @@ for idx, caminho_csv in enumerate(caminhos_arquivos, start=1):
 
     # Define o caminho completo para os arquivos .txt dentro da pasta "dados_processados"
     caminho_arquivo_originais = os.path.join(
-        caminho_pasta, f"dados_originais_M{idx}.txt"
+        caminho_pasta, f"dados_originais_C{idx}.txt"
     )
     caminho_arquivo_filtrados = os.path.join(
-        caminho_pasta, f"dados_filtrados_M{idx}.txt"
+        caminho_pasta, f"dados_filtrados_C{idx}.txt"
     )
 
     # Define o intervalo de tempo em segundos para escrita no arquivo.txt
@@ -112,42 +102,3 @@ for idx, caminho_csv in enumerate(caminhos_arquivos, start=1):
         for valor in filtered_data:
             arquivo_saida.write(f"{valor},{tempo_atual:.4f}\n")
             tempo_atual += intervalo_tempo
-    """
-    # Plotar um gráfico da curva original e da curva filtrada
-    plt.rcParams["font.family"] = ["DeJavu Serif"]
-    plt.rcParams["font.serif"] = ["Times New Roman"]
-
-    tam_font1 = 50
-
-    plt.figure(figsize=(60, 20))
-    plt.plot(tempo, dados, label=f"Dados originais M{idx}")
-    plt.plot(tempo, filtered_data, label=f"Dados filtrados M{idx}", linewidth=2)
-    plt.legend(fontsize=tam_font1)
-    plt.xlabel("Tempo (s)", fontsize=tam_font1)
-    plt.ylabel("Intensidade", fontsize=tam_font1)
-    plt.title(f"Filtro Passa-Baixo M{idx}", fontsize=tam_font1)
-    plt.xticks(fontsize=tam_font1)
-    plt.yticks(fontsize=tam_font1)
-    plt.grid(True)
-    plt.savefig(f"graficos/graf_dados_processados_filtrados_M{idx}.pdf", bbox_inches="tight")
-    # plt.show()
-
-    # Range do gráfico de recorte
-    num_pontos = 600
-
-    tam_font2 = 19
-
-    # Plotar um recorte dos dados originais e dos dados filtrados
-    plt.figure(figsize=(12, 6))
-    plt.plot(tempo[0:num_pontos], dados[0:num_pontos], label=f"Dados originais M{idx}")
-    plt.plot(tempo[0:num_pontos], filtered_data[0:num_pontos], label=f"Dados filtrados M{idx}", linewidth=3)
-    plt.legend(fontsize=tam_font2)
-    plt.xlabel("Tempo (s)", fontsize=tam_font2)
-    plt.ylabel("Intensidade", fontsize=tam_font2)
-    # plt.title(f"Recorte - Filtro Passa-Baixo M{idx}", fontsize=tam_font2)
-    plt.xticks(fontsize=tam_font2)
-    plt.yticks(fontsize=tam_font2)
-    plt.grid(True)
-    plt.savefig(f"graficos/graf_dados_processados_filtrados_recorte_M{idx}.pdf", bbox_inches="tight")
-    # plt.show()
-    """
